43dcb343924683ade4af70dd55b9a66c5978d9c5
Eddie-Pak <foalstn@naver.com>
2026-08-15 14:41:08 +0900

    build: Ktlint 도입

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,13 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.secrets.gradle.plugin) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+    }
 }
