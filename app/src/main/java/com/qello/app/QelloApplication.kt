615641238415385a4547c28532ed2dfffa84af2b
Eddie-Pak <foalstn@naver.com>
2026-08-15 16:47:18 +0900

    build: hilt annotation 추가

--- app/src/main/java/com/qello/app/QelloApplication.kt
+++ app/src/main/java/com/qello/app/QelloApplication.kt
@@ -1,7 +1,9 @@
 package com.qello.app
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class QelloApplication : Application() {
     override fun onCreate() {
         super.onCreate()
