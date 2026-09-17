1afdab0d9eff204d193793f3f9509da1f5b9b159
Eddie-Pak <foalstn@naver.com>
2026-09-18 03:22:50 +0900

    refactor: QelloBottomSheet 재사용

--- presentation/src/main/java/com/qello/presentation/ui/screen/main/MainScreen.kt
+++ presentation/src/main/java/com/qello/presentation/ui/screen/main/MainScreen.kt
@@ -1,17 +1,11 @@
 package com.qello.presentation.ui.screen.main
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.mapbox.geojson.Point
@@ -32,8 +25,10 @@ import com.mapbox.maps.extension.compose.style.standard.rememberStandardStyleSta
 import com.qello.presentation.R
 import com.qello.presentation.component.bottombar.HomeBottomBarTab
 import com.qello.presentation.component.bottombar.QelloBottomBarScaffold
+import com.qello.presentation.component.bottomsheet.QelloBottomSheet
 import com.qello.presentation.component.button.QelloIconButton
 import com.qello.presentation.component.button.QelloLargeButton
+import com.qello.presentation.component.spacer.QelloSpacer
 import com.qello.presentation.component.text.QelloText
 import com.qello.presentation.ui.designsystem.theme.QelloTheme
 
@@ -103,47 +98,35 @@ fun MainScreen(
 
         // 바텀시트
         if (showQuestionSendSheet) {
-            ModalBottomSheet(
+            QelloBottomSheet(
                 onDismissRequest = { showQuestionSendSheet = false },
-                containerColor = Color.Transparent,
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            QelloTheme.gradient.backgroundStrong,
-                            RoundedCornerShape(topStart = QelloTheme.radius.radius24, topEnd = QelloTheme.radius.radius24),
-                        )
-                        .padding(horizontal = QelloTheme.spacing.spacing20)
-                        .padding(vertical = 38.dp),
+                QelloText(
+                    text = "전 세계 사람들에게",
+                    style = QelloTheme.typography.heading1,
+                    color = QelloTheme.colors.label.strong,
+                )
+                QelloText(
+                    text = "질문을 보내보세요!",
+                    style = QelloTheme.typography.heading1,
+                    color = QelloTheme.colors.label.strong,
+                )
+
+                QelloSpacer(QelloTheme.spacing.spacing8)
+
+                QelloText(
+                    text = "궁금한 것을 질문하고, 다양한 사람들의 답변을 받아보세요.",
+                    style = QelloTheme.typography.caption1,
+                    color = QelloTheme.colors.label.normal2,
+                )
+
+                QelloSpacer(44.dp)
+
+                QelloLargeButton(
+                    text = "질문하러가기",
                 ) {
-                    QelloText(
-                        text = "전 세계 사람들에게",
-                        style = QelloTheme.typography.heading1,
-                        color = QelloTheme.colors.label.strong,
-                    )
-                    QelloText(
-                        text = "질문을 보내보세요!",
-                        style = QelloTheme.typography.heading1,
-                        color = QelloTheme.colors.label.strong,
-                    )
-
-                    Spacer(Modifier.height(QelloTheme.spacing.spacing8))
-
-                    QelloText(
-                        text = "궁금한 것을 질문하고, 다양한 사람들의 답변을 받아보세요.",
-                        style = QelloTheme.typography.caption1,
-                        color = QelloTheme.colors.label.normal2,
-                    )
-
-                    Spacer(Modifier.height(44.dp))
-
-                    QelloLargeButton(
-                        text = "질문하러가기",
-                    ) {
-                        showQuestionSendSheet = false
-                        onNavigateToQuestionCompose()
-                    }
+                    showQuestionSendSheet = false
+                    onNavigateToQuestionCompose()
                 }
             }
         }
