ee922d2c386bbbdfe83242d335be6002ce799215
Eddie-Pak <foalstn@naver.com>
2026-09-18 02:28:43 +0900

    feat: Timber DebugTree 추가

--- app/src/main/java/com/qello/app/QelloApplication.kt
+++ app/src/main/java/com/qello/app/QelloApplication.kt
@@ -3,11 +3,16 @@ package com.qello.app
 import android.app.Application
 import com.mapbox.common.MapboxOptions
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class QelloApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         MapboxOptions.accessToken = BuildConfig.MAPBOX_ACCESS_TOKEN
     }
